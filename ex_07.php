d6e8267ba9189574607dd76d6c314fd51c891f2a
damienn7 <118727505+damienn7@users.noreply.github.com>
2022-11-24 14:06:32 +0100

    Ajout des exercices 7 et 8

--- ex_07.php
+++ ex_07.php
@@ -1,1 +1,13 @@
 <?php
+
+function aff_array($arr)
+{
+    $count = 0;
+    foreach ($arr as $key => $value)
+    {
+        echo "{$value}\n";
+        $count++;
+    }
+
+    return $count;
+}
--- ex_08.php
+++ ex_08.php
@@ -1,1 +1,9 @@
 <?php
+
+function print_array_with_key(array $my_array)
+{
+    foreach ($my_array as $key => $value)
+    {
+        print "{$key} => {$value}\n";
+    }
+}
