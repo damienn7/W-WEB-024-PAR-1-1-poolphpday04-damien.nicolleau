2b16e9ea0b9977c4f2a732d8f9e4095a4699378a
damienn7 <118727505+damienn7@users.noreply.github.com>
2022-11-24 15:28:13 +0100

    Remplacement de la fleche (=>) par les deux points

--- ex_08.php
+++ ex_08.php
@@ -4,6 +4,6 @@ function print_array_with_key(array $my_array)
 {
     foreach ($my_array as $key => $value)
     {
-        print "{$key} => {$value}\n";
+        print "{$key} : {$value}\n";
     }
 }
