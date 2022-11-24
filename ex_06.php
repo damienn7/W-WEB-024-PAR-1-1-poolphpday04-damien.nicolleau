639bcb525427c4198c1ae03c0244e6cd7e55158d
damienn7 <damien.nicolleau@epitech.eu>
2022-11-24 12:34:24 +0100

    Ajout de l'exercice 6

--- ex_06.php
+++ ex_06.php
@@ -1,1 +1,12 @@
 <?php
+
+function get_angry_bird(int $nbr) 
+{   $piou="piou";
+    $chaine="";
+    for ($i=0;$i<$nbr;$i++)
+    {
+        $chaine=$chaine.$piou;
+    }
+    $chaine=$chaine."\n";
+    echo $chaine;
+}
