08b7d4b142c06f6bfc6aaab31117532292227f6e
damienn7 <118727505+damienn7@users.noreply.github.com>
2022-11-24 19:07:17 +0100

    modification de l'exercice 12

--- ex_12.php
+++ ex_12.php
@@ -3,9 +3,14 @@
 function is_similar($a, $b)
 {
     switch($a):
+        case $a == $b && gettype($a) == gettype($b):
+            print "Salme type and value.\n";
+            break;
         case $a == $b:
             print "Same value\n";
+            break;
         case gettype($a) == gettype($b):
             print "Same type\n";
+            break; 
     endswitch;
 }
